b088977eb0b2b7c762006429cb8b6aac29c26324
Hamza Lahrach <hlahrach@e1r3p7.1337.ma>
2022-06-12 22:30:31 +0100

    ADD : test  2 and add some tests to the test 1

--- philo.sh
+++ philo.sh
@@ -1,3 +1,4 @@
+
 #!bin/bash
 
 Black='\033[0;30m'        # Black
@@ -11,17 +12,45 @@ White='\033[0;37m'        # White
 
 test_1 ()
 {
+		echo -en "${Purple}test 1 : "
 		output=$("$1" 3 310 200 100 > log && sleep 2 ; pkill "$1" ; cat log | grep died -m 1 | awk '{print $NF}')
 		#echo $output
 		if [ "$output" == "died" ]
 				then
 						echo -e "${Green}OK${White}"
+		else
+						echo -e "${Red}KO${White}"
+		output=$("$1" 3 800 400 400 > log && sleep 0.9 ; pkill "$1" ; cat log | grep died -m 1 | awk '{print $NF}')
+
+		rm -f log
 		fi
 }
-# test_2 ()
-# {
 
-# }
+test_2 ()
+{
+		"$1" 3 800 200 200 > log &
+		i=1
+		check=0
+		while [ $i -lt 5 ]
+		do
+				pgrep philo > /dev/null
+				if [ $? != 0 ]
+				then
+						check=1
+						echo -e "${Red}KO${White}"
+						pkill "$1" $2>/dev/null  
+						break ;
+				fi
+				sleep 1
+				i=$((i + 1)) 
+		done 
+		if [ $check == 0 ]
+				then
+						echo -e "${Green}OK${Withe}"
+						pkill "$1" &2>/dev/null 
+		fi
+		#rm -f log
+}
 # test_3 ()
 # {
 
@@ -52,4 +81,5 @@ else
 		exit 1
 fi
 #echo $file_name
-test_1 "$file_name" 
+test_1 "$file_name"
+#test_2 "$file_name"

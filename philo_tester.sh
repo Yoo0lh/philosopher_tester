1f45a33b36397d6a6bc8cea50702bc5bf5cf184f
Hamza Lahrach <hlahrach@e1r1p8.1337.ma>
2022-06-19 16:51:29 +0100

    FINISH : the test

--- philo_tester.sh
+++ philo_tester.sh
@@ -1,5 +1,4 @@
-#!bin/bash
-
+#!/bin/bash
 Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
@@ -8,6 +7,8 @@ Blue='\033[0;34m'         # Blue
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
+UCyan='\033[4;36m'        # Cyan
+BCyan='\033[1;36m'        # Cyan
 SP="  "
 file_output="output"
 optin="philo"
@@ -19,7 +20,7 @@ test_1 ()
 		output=$("$1" 3 800 200 200 > log &&  cat log | grep died -m 1 | awk '{print $NF}') &
 		i=0
 		check=0
-		while [ $i -le 10 ] #|| [ "$output" == "died" ]
+		while [ $i -le 16 ] 
 		do
 				pgrep "$2" > /dev/null
 				if [ $? !=  0 ] || [ "$output" == "died" ]
@@ -40,7 +41,7 @@ test_1 ()
 		output=$("$1" 10 800 200 200 > log2 ; cat log2 | grep died -m 1 | awk '{print $NF}') &
 		i=0
 		check=0
-		while [ "$i" -le 10 ]
+		while [ "$i" -le 16 ]
 		do
 				pgrep "$2" > /dev/null
 				if [ $? !=  0 ] || [ "$output" == "died" ]
@@ -58,13 +59,13 @@ test_1 ()
 		fi
     pkill "$2" > /dev/null
 		#test 2 / 3
-    output=$("$1" 100 800 200 200 > log3 ; sleep 2; cat log3 | grep died -m 1 | awk '{print $NF}')
+    output=$("$1" 100 800 200 200 > log3 ; sleep 2; cat log3 | grep died -m 1 | awk '{print $NF}') &
 		i=0
 		check=0
-    while [ $i -le 10 ]
+    while [ $i -le 16 ]
 		do
 				pgrep "$2" > /dev/null  
-				if [ "$?" -ne  0 ] #|| [ "$output" == "died" ]
+				if [ "$?" -ne  0 ] || [ "$output" == "died" ]
 				then
 						check=1
 						echo -ne "${SP}${Red}KO${White}"
@@ -90,12 +91,11 @@ test_2 ()
     output=$("$1" 4 410 200 200 > log ; cat log | grep died -m 1 | awk '{print $NF}') &
 		i=0
 		check=0
-		while [ $i -le 10 ]
+		while [ $i -le 16 ]
 		do
 				pgrep "$2" > /dev/null
 				if [ "$?" -ne 0 ] || [ "$output" == "died" ] 
 				then
-						#echo "$output"
 						check=1
 						echo  -en "${SP}${Red}KO${White}"
 						break ;
@@ -108,18 +108,17 @@ test_2 ()
 						echo -en "${SP}${Green}OK${White}"
 		fi
 		
-		pkill "$1" &2> /dev/null
+		#pkill "$1" &2> /dev/null
 		#test 3 / 2
 		output=$("$1" 5 800 200 200 > log2 ; cat log | grep died -m 1 | awk '{print $NF}') &
 		#"$1" 5 800 200 200 > log  &
 		i=0
 		check=0
-		while [ $i -le  10 ]
+		while [ $i -le  16 ]
 		do
 				pgrep  "$2" > /dev/null
 				if [ "$?" -ne 0 ] || [ "$output" == "died" ]
 				then
-						#echo  "$output"
 						check=1
 						echo  -en "${SP}${Red}KO${White}"
 						break ;
@@ -131,16 +130,16 @@ test_2 ()
 				then
 						echo -en "${SP}${Green}OK${White}"
 		fi
-    pkill "$2" 2&> /dev/null
+    #pkill "$2" 2&> /dev/null
+		
 		output=$("$1" 20 800 200 200 > log3 && cat log | grep died -m 1 | awk '{print $NF}') &
 		i=0
 		check=0
-		while [ $i -le  10 ]
+		while [ $i -le  16 ]
 		do
 				pgrep  "$2" > /dev/null
 				if [ "$?" -ne 0 ] || [ "$output" == "died" ]
 				then
-						#echo  "$output"
 						check=1
 						echo  -en "${SP}${Red}KO${White}"
 						break ;
@@ -154,7 +153,6 @@ test_2 ()
 		fi
 		pkill "$2" &2> /dev/null
 		echo " "
-		#rm -f $file_output
 }
 
 test_3 ()
@@ -214,12 +212,7 @@ test_4 ()
 {
 		echo -en "${Purple}test 4 :"
 		#test 1 / 1
-		output=$("$1" 3 310 200 100 > log && sleep 4 ; pkill "$2" ; cat log | grep died -m 1 | awk '{print $NF}') 
-		# ("$1" 3 310 200 200 > log) &&
-		# sleep 5
-		# pkill $1 > /dev/null 
-		# output=$(cat log | grep died -m 1 | awk '{print $NF}')
-		#echo $output
+		output=$("$1" 3 310 200 100 >> log ; sleep 1 ; pkill "$2" ; cat log | grep died -m 1 | awk '{print $NF}') 
 		if [ "$output" == "died" ]
 				then
 						echo -en "${SP}${Green}OK${White}"
@@ -227,7 +220,7 @@ test_4 ()
 						echo -en "${SP}${Red}KO${White}"
 		fi
 		#test 2 / 1
-		output=$("$1" 3 800 400 400 > log2 ; sleep 0.9 ; pkill "$2" ; cat log2 | grep died -m 1 | awk '{print $NF}')
+		output=$("$1" 3 800 400 400 > log2 ; sleep 0.9 ; pkill "$2" ; cat log2 | grep died -m 1| awk '{print $NF}')
 		if [ "$output" == "died" ]
 				then
 						echo -en "${SP}${Green}OK${White}"
@@ -235,7 +228,7 @@ test_4 ()
 						echo -en "${SP}${Red}KO${White}"
 		#test 3 / 1
 		fi
-		output=$("$1" 10 800 400 200 > log3 ; sleep 0.9 ; pkill "$2" ; cat log3 | grep died -m 1 | awk '{print $NF}')
+		output=$("$1" 10 800 400 200 > log3 ; sleep 0.9 ; pkill "$2" ; cat log3 | grep died -m 1| awk '{print $NF}')
 		if [ "$output" == "died" ]
 				then
 						echo -en "${SP}${Green}OK${White}"
@@ -243,8 +236,8 @@ test_4 ()
 						echo -en "${SP}${Red}KO${White}"
 		fi
 		echo " "
+		pkill "$2" > /dev/null
 }
-
 
 # test_5 ()
 # {
@@ -255,24 +248,33 @@ test_4 ()
 
 # }
 
-if [ "$1" == "m" ]
+if [ ${#@} -ne 0 ] && [ "${@#"--help"}" = "" ]; then
+  printf -- 'usage : \n\t./philo_tester [mode] \n\tm : for mandatory part\n\tb : for bonus part\n';
+  exit 0;
+elif [ "$1" == "m" ]
 		then
-        echo "im in the mandatory part ..."
 				make -C ../philo/ > /dev/null 
 				file_name="../philo/philo"
         optin="philo"
+		echo -e "${Cyan}===============================${Purple} MANDATORU ${Cyan}=============================== ${White}"
+		echo -e "${Yellow}[the tests might take time please wait ...]${White}"
+		test_1 "$file_name" "$optin"
+		test_4 "$file_name" "$optin"
+		test_3 "$file_name" "$optin"
+		test_2 "$file_name" "$optin"
+		echo -e "${Cyan}==================================${Purple} END ${Cyan}================================== ${White}"
+		rm -f log log3 log2
 elif [ "$1" == "b" ]
 		then
-        echo "im in the bonus part "
 				make -C ../philo_bonus/ > /dev/null 
 				file_name="../philo_bonus/philo_bonus"
         optin="philo_bonus"
+				echo -e "${Cyan}===============================${Purple} BONUS ${Cyan}=============================== ${White}"
+				echo -e "${Yellow}[the tests might take time please wait ...]${White}"
+				test_1 "$file_name" "$optin"
+				test_2 "$file_name" "$optin" 
+				echo -e "${Cyan}==================================${Purple} END ${Cyan}================================== ${White}"
 else
 		echo "ERROR  arguments"
 		exit 1
 fi
-echo "the fikle name is $file_name"
-test_1 "$file_name" "$optin"  ; sleep 1 ; make re -C ../"$optin" > /dev/null; test_2 "$file_name" "$optin"
-#test_3 "$file_name" "$optin"
-#test_4 "$file_name" "$optin"
-# rm -f log log2 log3
